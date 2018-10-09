3d64952f3bd46c26c852b3db1c09cf1e3ead95d0
Tory777 <42585771+Tory777@users.noreply.github.com>
2018-10-09 21:49:22 +0300

    Update script.js

--- JS2/Loops-statements/script.js
+++ JS2/Loops-statements/script.js
@@ -1,1 +1,16 @@
+for (var i = 0; i < 15; i++) {
+  if(i%2==0){
+    alert(i,' is even');
+  }
+  else if(i%2!=0){
+    alert(i,' is odd');
+  }}
 
+for (var i = 1; i < 101; i++) {
+  if(i%3==0 && i%5==0){
+    console.log(i,'FizzBuzz');}
+  else if(i%3==0){
+    console.log(i,'Fizz');}
+  else if(i%5==0){
+    console.log(i,'Buzz');}
+}
