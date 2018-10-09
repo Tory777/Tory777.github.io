c9d8e815d201e750a35c2527ac7a29c5e66875c5
Tory777 <42585771+Tory777@users.noreply.github.com>
2018-10-09 22:47:56 +0300

    Update script.js

--- JS2/Loops-statements/script.js
+++ JS2/Loops-statements/script.js
@@ -1,3 +1,4 @@
+//task 1
 for (var i = 0; i < 15; i++) {
   if(i%2==0){
     alert(i,' is even');
@@ -6,6 +7,7 @@ for (var i = 0; i < 15; i++) {
     alert(i,' is odd');
   }}
 
+//task2
 for (var i = 1; i < 101; i++) {
   if(i%3==0 && i%5==0){
     console.log(i,'FizzBuzz');}
@@ -14,3 +16,28 @@ for (var i = 1; i < 101; i++) {
   else if(i%5==0){
     console.log(i,'Buzz');}
 }
+//task 3
+
+
+//task 4
+var summa=0; 
+for (var i = 1; i < 1000; i++) {
+  if(i%3==0 && i%5==0){
+    summa+=i;
+    }
+}
+console.log(summa);
+
+//task 5
+var summa=0; 
+for (var i = 10; i <= 90; i++) {
+  if(i%3==0 && i%5==0){
+   console.log(i);}
+}
+// if you meant different numbers: numbers that divide to 3 and separate numbers that divide to 5 than next code is true
+/*for (var i = 10; i <= 90; i++) {
+  if(i%3==0){
+   console.log(i);}
+else if(i%5==0){
+   console.log(i);}
+}*/
