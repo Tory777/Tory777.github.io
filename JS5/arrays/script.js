bb721048cd0586739c52bcaaf1b795d258af8ba5
Tory777 <42585771+Tory777@users.noreply.github.com>
2018-11-05 14:35:30 +0200

    Update script.js

--- JS5/arrays/script.js
+++ JS5/arrays/script.js
@@ -32,13 +32,26 @@ function isPalindrome(str) {
   document.write('isPalindrome result: ' + 'str - ' + test + ' str2 - ' + test2);
 
 
-/*3 задание
+/*3 задание  +
 Анаграммы — слова, состоящие из одинакового количества одинаковых букв, но в разном порядке. Например:
 воз - зов, киборг - гробик, корсет - костер - сектор,
 
 Напишите функцию anClean(arr), которая возвращает массив слов, очищенный от анаграмм.
 var arr = ['воз', 'киборг', 'корсет', 'ЗОВ', 'гробик', 'костер', 'сектор'];
 console.log( anClean(arr) ); // 'воз,киборг,корсет' или 'ЗОВ,гробик,сектор'*/ 
+
+var myarr = ['воз', 'киборг', 'корсет', 'ЗОВ', 'гробик', 'костер', 'сектор'];
+function anClean(arr){
+  var objReform = {};
+  for (var i = 0; i< arr.length; i++){
+      var reform = arr[i].toLowerCase().split('').sort().join('');
+      objReform[reform] = arr[i];
+      console.log(objReform)}
+  var result = [];
+  for (var key in objReform) result.push(objReform[key]);{
+    return result;}
+}
+console.log(anClean(myarr));
 
 
 /*4 задание +
@@ -78,30 +91,30 @@ function sumEven(num) {
 }
 console.log(sumEven(n))
 
-/*6 задание
+/*6 задание  +
 Напишите функцию callMe которая будет принимать 3 аругумента, это будут цыфры, 
 потом она будет суммировать их и выводить на екран результат их суммы!
 Если в нее передели не числа то она должна выводить сообщение об ошибке в консоль и возвращать null; 
 (вывод ошибки в консоль осуществялется через console.error("Тут текст ошибки") ) */
 
-var num1 = 1  
-var num2 = 2
-var num3 = 3
+var num1 = 10  
+var num2 = 'df'
+var num3 = 333
 function callMe(a,b,c){
-    if(typeof a == Number & typeof b == Number & typeof c == Number){
+    if(typeof a == 'number' & typeof b == 'number' & typeof c == 'number'){
       var res = a + b + c
       return res;
     }
-  else{return false}
-}
-console.log(callMe(1,5,8))
+   else {console.error('Sorry, some items does not seem to be a number');
+       return false}}
+console.log(callMe(num1, num2, num3))
 
 
 /*7 задание
 Напишите функцию callMeAgain которая будет принимать 1 аргумент, который будет массивом. 
 Она должна возвращать массив соедененный через запятые и отсортированный по алфавиту (Array.join(','));  */
 
-/*8 задание
+/*8 задание  + 
 Дан массив объектов:
 
 var arr = [
@@ -136,12 +149,34 @@ for(var i = 0; i < arr.length; i++) {
 чтобы все элементы в массиве остались отсортированные по полю age
 Если такой пользователь уже существует в массиве верните пользователю сообщение: "Такой пользователь уже существует"  */
 
-/*10 task
+/*10 task   +
 Дан массив с числами. Подсчитайте количество цифр 3 в данном массиве.
 Пример: [13, 35, 3, 443] - в массиве 4 цифры 3.  */
 
-/*11 task
+arr  = [13, 35, 3, 443];
+var counter = 0;
+str = arr.join('')
+ for (var i = 0; i< str.length; i++){
+  if (str[i]==3){
+    counter++
+  }
+}
+console.log(counter)
+
+/*11 task   + 
 Дан массив ['1', '2', '3', '4', '5', '6']. Сделайте из него строку '16-25-34'. Массив, конечно же, может быть любым.  */
+
+arr  =  ['1', '2', '3', '4', '5', '6'] 
+str = arr.join('')
+console.log(str)
+ var newStr = ''
+ var myArr = []
+ var j = str.length;
+ for (var i = 0; i< str.length/2; i++){
+    myArr.push(str.slice(i,i+1)+str.slice(str.length-i-1,str.length-i));
+    newStr = myArr.join('-')
+}
+console.log(newStr)
 
 /*12 task
 Даны два массива, к примеру
@@ -150,14 +185,23 @@ for(var i = 0; i < arr.length; i++) {
 Создайте новый массив, заполненный суммами соответствующих элементов, то есть [1+4, 2+5, 3+6] = [5, 7, 9].
 Скрипт должен работать для любых массивов, даже если их длина не одинакова (подумайте, что делать в этом случае). */
 
-/*13 task
+/*13 task  no work
 Дан массив. Напишите функцию, которая будет удалять элемент из массива по его тексту.
 Пример: arr = [1,2,3,4,3,3]. Удалим все элементы с содержимым 3: func(arr, 3).  */
 
-14 task
-Реализуйте функцию intersection, вернёт массив из элементов, встречающихся в каждом из переданных массивов.
+arr  = [13, 35, 12, 3, 443, 55];
+ for (var i = 0; i< arr.length; i++){
+   for (var j = 0; j< arr[i].length; i++){
+           if (arr[i][j]==3){
+           console.log(arr[i][j])
+           arr.splice(i)}} 
+}
+console.log(arr)
 
-Пример: intersection([1, 2, 3], [101, 2, 1, 10], [2, 1]) вернёт [1, 2].
+
+/*14 task
+Реализуйте функцию intersection, вернёт массив из элементов, встречающихся в каждом из переданных массивов.
+Пример: intersection([1, 2, 3], [101, 2, 1, 10], [2, 1]) вернёт [1, 2].*/
 
 15 task
 Дан массив с числами, к примеру [2, 3, 1, 4]. Сделайте из него массив [2, 2, 3, 3, 3, 1, 4, 4, 4, 4]. В массиве могут быть только целые положительные числа.
